3ec72fe2ea4d088cf56baeccfced0a547edcaec4
anfnamazi <anf.namazi@gmail.com>
2024-04-06 01:52:03 +0330

    feat: add new.target key to new pattern

--- pattern/new.js
+++ pattern/new.js
@@ -1,11 +1,12 @@
 function Person(name, age) {
   this.name = name;
   this.age = age;
+  this.target = new.target;
 }
 
 Person("ag", 12);
 
-console.log(name, age);
+console.log(name, age, target);
 
 const person = new Person("tb", 24);
 
