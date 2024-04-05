2d6c93252bc2e943873efa2cf8cf4d314fb23d93
anfnamazi <anf.namazi@gmail.com>
2024-04-06 01:48:04 +0330

    feat: test class func with new & without new key

---
+++ pattern/new.js
@@ -0,0 +1,12 @@
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person("ag", 12);
+
+console.log(name, age);
+
+const person = new Person("tb", 24);
+
+console.log(person);
